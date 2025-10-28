d85e5a1e753b6ad504466a7645133c75542f6487
csolanoave <csolano@asertec.com.ec>
2025-10-28 12:56:22 -0500

    Mantenimiento sin rutas

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../services/user.service';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'user-app',
@@ -13,9 +14,11 @@ import { UserFormComponent } from './user-form/user-form.component';
 })
 export class UserAppComponent implements OnInit {
   title: string = 'Listado de Usuarios';
-  users: User[] = []; // This should ideally be typed with a User model
+  users: User[] = []; 
   userSelected: User;
-  constructor(private service: UserService) {
+  open: boolean = false;
+  constructor(
+    private readonly service: UserService) {
     this.userSelected = new User();
     // Initialization logic can go here
   }
@@ -30,14 +33,44 @@ export class UserAppComponent implements OnInit {
     if (user.id > 0) {
       this.users = this.users.map((u) => (u.id === user.id ? { ...user } : u));
     } else {
-      this.users = [...this.users, { ...user, id: new Date().getTime() }];
+      this.users = [...this.users, { ...user, id: Date.now() }];
     }
     this.userSelected = new User();
+    Swal.fire({
+      title: "Guardado!",
+      text: "Usuario guardado correctamente!",
+      icon: "success"
+    });
+    this.setOpen();
   }
   removeUser(id: number): void {
-    this.users = this.users.filter((user) => user.id !== id);
+    console.log('Eliminando usuario con id:', id);
+    Swal.fire({
+      title: "Estas seguro?",
+      text: "No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminarlo!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.users = this.users.filter((user) => user.id !== id);
+        Swal.fire({
+          title: "Eliminado!",
+          text: "Usuario eliminado correctamente!",
+          icon: "success"
+        });
+      }
+    });
   }
   setSelectedUser(userRow: User): void {
     this.userSelected = { ...userRow };
+    this.open = true;
   }
+
+  setOpen(): void {
+    this.open = !this.open;
+  }
+
 }
